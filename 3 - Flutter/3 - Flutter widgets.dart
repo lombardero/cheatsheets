7c0ade0e11616a8d0083bbc7d98eb0af2758d4ec
lombardero <manuel.lombardero.botey@gmail.com>
2019-11-30 21:47:29 -0500

    update

--- 3 - Flutter/3 - Flutter widgets.dart
+++ 3 - Flutter/3 - Flutter widgets.dart
@@ -16,6 +16,7 @@
 
 // Reminder: press Ctlr + Space in VS code to see the arguments
 //           use Alt + Shift + F to auto-format the code
+//           use ^ Shift R to make Refactor suggestions
 
 RUNNING THE WIDGETS
     void main() {
@@ -30,7 +31,7 @@ RUNNING THE WIDGETS
         //-> does the same as the code above, in one line of code
         //   (different and often used syntax)
 
-WIDGETS: EXTENDING DART CLASSES
+WIDGETS: STATEFUL vs STATELESS, EXTENDING DART CLASSES
   // Extensions are used to build widgets from Flutter's pre-defined
   // objects. To define a widget we need to extend the StatelessWidget
   // or StatefulWidget class, and override its 'build' function'
@@ -40,7 +41,14 @@ WIDGETS: EXTENDING DART CLASSES
       // yaml file declares the dependencies, and here we bring out the
       // widgets from this file, which we can then use to extend our own
       // classes
+
+  // ------- Stateless Widgets ------
+  // For Stateless widgets, we need to import or extend only one class
   class MyWidget extends StatelessWidget {
+    'all class variables and functions used on the code go here'
+    // the names can be re-used later (careful -> in this example we are
+    // using a Stateless Widget! therefore it cannot change -> does not
+    // usually need functions or variables -> stateful)
     @override
     Widget build(BuildContext context) {
       return MaterialApp(home: Text('Hello world!'));
@@ -48,23 +56,67 @@ WIDGETS: EXTENDING DART CLASSES
   }
       //-> this code allows us to create a new class 'MyWidget', built from
       //   StatelessWidget (the most basic Flutter widget).
-      // Note0: the '@override' decorator is there to make our code more
+      // Note0: Stateless Widgets can only change once input data (from outside
+      //   the class) is inserted. It does not keep internal state, and therefore
+      //   will never get re-rendered based on state.
+      // Note1: the '@override' decorator is there to make our code more
       //   readable (good practice). It is a Flutter decorator that says
       //   we are 'overriding' or 're-writing' any of the in-built functions
       //   of the 'StatelessWidget' class (in this case, the 'build()'
       //   function). Flutter actually forces us to override it, so it is
       //   not required (code would still work) but it adds readability
       //   (make sure we are not accidentally overriding)
-      // Note1: StatelessWidget needs us to define the 'build()' function
+      // Note2: StatelessWidget needs us to define the 'build()' function
       //   to work (which is called by Flutter when the Widget is loaded).
-      // Note 2: the function 'build()', expects a 'context' argument,
+      // Note3: the function 'build()', expects a 'context' argument,
       //   which is a 'BuildContext' class (defined by Flutter), and
       //   will return a 'Widget' (mentioned before the function name), since
       //   it will output something on the UI. 'build()' is what will be
       //   called to output the UI when 'MyWidget' is run.
-      // Note 3: 'MaterialApp' is a Google pre-defined widget (accepts args),
+      // Note4: 'MaterialApp' is a Google pre-defined widget (accepts args),
       //   which requires us to define the argument 'home' to be run. 'home'
       //   expects a Widget too. In this case, we will put some text.
+  
+  // ------ Stateful Widgets ------
+  // For stateful widgets, we need to create and import from 'material.dart'
+  // TWO defined classes: the StatefulWidget AND its State. The State class
+  // will keep the state stored. We need to connect both StatefulWidget and
+  // state together as follows:
+  class MyWidget extends StatefulWidget {
+    @override
+    State<StatefulWidget> createState() {
+      return MyWidgetState();
+    }
+  }
+      //-> We first define the StatefulWidget class, and override its State
+      //   class by returning 'MyWidgetState()' executed with the 'createState()'
+      //   method, which outputs a State object: 'State<StatefulWidget>'
+      // Note: this class will return the State of the Widget, which will
+      //   ask the app to be re-rendered every time the setState() function
+      //   is required to be run.
+  class MyWidgetState extends State<MyWidget> {
+    var var_used_in_state = 0;
+    setState(() {
+      var_used_in_state = var_used_in_state + 1;
+    })
+
+    @override
+    Widget build(BuildContext context) {
+      ....
+    }
+  }
+      //-> In the State part, we need to add a pointer to the 'MyWidget' widget
+      //   by adding <MyWidget> after extending the State class: it is extending
+      //   the State of MyWidget.
+      //-> In the State definition, we need to add the variables and functions 
+      //   that will affect our state so that flutter knows WHEN it needs to
+      //   re-render the App (otherwise it would be super slow, re-rendering
+      //   anytime the user inputs something.
+      //-> The rest of the application is kept inside the State class
+      // Note: setState() is a method that takes in a void function as an
+      //   argument, with all the State-modifying elements on it.
+      //   setState() will re-run the 'build()' method every time it is
+      //   triggered.
 
 OUTPUT & INPUT WIDGETS (BASIC, VISIBLE)
 // These widgets are 'Visible', and output something on the UI, they
@@ -87,9 +139,34 @@ OUTPUT & INPUT WIDGETS (BASIC, VISIBLE)
           //   color, whatever we want)
         onPressed:
           //-> defines the action to be taken if the button is
-          //   pressed.
-            onPressed: null // will disable the button (will not
-                            // do anything)
+          //   pressed. It expects a void function, which can be
+          //   defined right on the spot, or named and re-used in
+          //   many buttons
+            onPressed: () => print('Button pressed!')
+              // 
+            onPressed: function 
+              // onPressed expects a function (which needs to be
+              // the name itself without parenthesis, since otherwise
+              // Flutter will execute it when the Widget is built, and
+              // we only want to execute it once the button is pressed
+              // Note: 'function' should be defined inside of the class
+              // where it belongs to, usually before the '@override' of
+              // the 'build' function.
+              void function() {
+                // do something
+                print('Button pressed!')
+              }
+                //-> will create a function that prints 'Button pressed!'
+                //   on the logger of the terminal (do not see in the app)
+            onPressed: () => print('Button pressed!')
+              //-> generates a nameless function directly for the button
+              //   (takes no arguments and prints something)
+            onPressed: () {
+              print('Button pressed!')
+              }
+              //-> same as preceding line (different syntax)
+            onPressed: null 
+              // will disable the button (will not do anything)
 
     Card()
 
