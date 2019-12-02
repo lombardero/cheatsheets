79c742fb68a3e0623e26f1452bb912fdd5fef05d
lombardero <manuel.lombardero.botey@gmail.com>
2019-12-01 22:26:50 -0500

    updating

--- 3 - Flutter/Google meetup annot.dart
+++ 3 - Flutter/Google meetup annot.dart
@@ -1,17 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:folding_cell/folding_cell.dart';
 
 
-class PuzzleFour extends StatelessWidget {
-  final _foldingCellKey = GlobalKey<SimpleFoldingCellState>();
-  void onGroupPressed(BuildContext context) => Navigator.pop(context);
-  Color hexToColor(String code) => Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
-  
-  @override
-  Widget build(BuildContext context) {
-
-  Widget _buildInnerBottomWidget() {
     //....
         // copy from Google meetup
           Transform.rotate( // create a transformation (rotation), which will be applied to the child
@@ -33,16 +23,38 @@ class PuzzleFour extends StatelessWidget {
             )
           // different thing:
           AspectRatio( // allows you to leave some space between "widgets"? (buttons) : apply to each
+          // with the same aspectRatio for the same spacing
             aspectRatio: 4.0/3
             child: activeTab()
           )
           )
         ),
       ),
-      
+      Drawer(
+        child: CustomPaint(c))
     );
   }
   
+}
+// extend class to create the background paint for the drawer (which we want orange and with circles)
+class DrawerPainter extends CustomPainer {
+
+  DrawePainter() : backgroundPaint = Paint()..color = const Colot(0xFFEA224)
+  smallCirclePain = Paint()..color = const Color(0xFFF4C67E) // Create an instance to paint a small circle
+  // double points chains methods together
+  // example
+  // Class()..method1
+  //      ..method2
+
+  final Paint smallCirclePaint;
+
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.clipRect(Rect.fromLTWH(....)) // avoids the figures to 'go further' than the limit
+                                         // of the rectange (in thei case, the droor)
+    canvas.drawRect(Rect,fromLTWH(0,0,size.width,.....))
+  }
 }
 // create this to create a "diamond button" widget (widget, sure?)
 class DiamondButton extends StatelessWidget  {
